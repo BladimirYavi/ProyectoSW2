90773066c10f0fa0b581883f64d8ffd5a3042e20
Matias Flores <arnod7991@gmail.com>
2019-11-28 17:34:50 -0400

    make it better

--- Dato/Conexion.php
+++ Dato/Conexion.php
@@ -2,10 +2,10 @@
 class Conexion{
 
     public function conectar(){
-        $host		= "ec2-174-129-253-1.compute-1.amazonaws.com";
-        $dbname 	= "d2qoekql14tmvk";
-        $usuario 	= "lfilrbxbydspcq";
-        $password	= "c467d996ccc04d2c41876e32cb889dfc1989f85b21ed97e9424e63749a298133";
+        $host		= "ec2-174-129-255-106.compute-1.amazonaws.com";
+        $dbname 	= "d32br04ka132tr";
+        $usuario 	= "syujivhbfoulgi";
+        $password	= "8e5ceb4085d0ab99c9ee8e607885d2ea41b8870fca2c0f241534c6bc7599c0ad";
 
         $link = new PDO("pgsql:host=$host;dbname=$dbname",$usuario,$password);
         return $link;
